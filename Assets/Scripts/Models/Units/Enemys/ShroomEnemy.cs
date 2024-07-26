867e40e6cd5f1cc4c7d1c1f0be930b63adb3495c
b0l0d1a <110717372+Volandimer1@users.noreply.github.com>
2024-07-26 17:03:25 +0300

    TEST

---
+++ Assets/Scripts/Models/Units/Enemys/ShroomEnemy.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class ShroomEnemy : MonoBehaviour, ITargetable
+{
+    [SerializeField] private float _delayBeforeLeaving = 5f;
+
+    private float _timer = 0f;
+
+    public void Initialize(Vector3 position)
+    {
+        transform.position = position;
+    }
+
+    public void ExecuteHitEction(int damage)
+    {
+        Debug.Log($"{gameObject.name} took {damage} damage");
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= _delayBeforeLeaving)
+        {
+            Debug.Log($"{gameObject.name} leaving");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        IHaveDamage damager;
+
+        if (other.gameObject.TryGetComponent<IHaveDamage>(out damager))
+        {
+            ExecuteHitEction(damager.GetDamageValue());
+        }
+    }
+}
